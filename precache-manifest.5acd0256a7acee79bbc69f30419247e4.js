0d81a8b8e4faff06938818f9293f282c69f14322
Oxa <oxakost@gmail.com>
2020-05-01 01:38:13 +0300

    Updates

---
+++ precache-manifest.5acd0256a7acee79bbc69f30419247e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a694a547de56fbc119b7843cbc0c458",
+    "url": "/ignat-hw-ref/index.html"
+  },
+  {
+    "revision": "75cda91f2984324accb5",
+    "url": "/ignat-hw-ref/static/css/main.e1f39813.chunk.css"
+  },
+  {
+    "revision": "9d4fe9404e81ecd87718",
+    "url": "/ignat-hw-ref/static/js/2.5bd34e42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ignat-hw-ref/static/js/2.5bd34e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cda91f2984324accb5",
+    "url": "/ignat-hw-ref/static/js/main.98734a5b.chunk.js"
+  },
+  {
+    "revision": "1b7b19e29105a112945d",
+    "url": "/ignat-hw-ref/static/js/runtime-main.23389698.js"
+  },
+  {
+    "revision": "adf3f16bee2eb2672ecd8cf3c1315595",
+    "url": "/ignat-hw-ref/static/media/avatar.adf3f16b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ignat-hw-ref/static/media/logo.5d5d9eef.svg"
+  }
+]);
